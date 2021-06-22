91e447de27a0ed4ebbc27247e999abc1ca35edb2
Oguz Konya <oguzkonya34@gmail.com>
2021-06-22 23:39:44 +0100

    Add horizontal movement system

--- Assets/Generated/Game/GameComponentsLookup.cs
+++ Assets/Generated/Game/GameComponentsLookup.cs
@@ -8,15 +8,18 @@
 //------------------------------------------------------------------------------
 public static class GameComponentsLookup {
 
+    public const int HorizontallyMoving = 0;
+    public const int Position = 1;
 
-
-    public const int TotalComponents = 0;
+    public const int TotalComponents = 2;
 
     public static readonly string[] componentNames = {
-
+        "HorizontallyMoving",
+        "Position"
     };
 
     public static readonly System.Type[] componentTypes = {
-
+        typeof(HorizontallyMovingComponent),
+        typeof(PositionComponent)
     };
 }
---
+++ Assets/Scripts/Systems/Movement/HorizontalMovementSystem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Entitas;
+
+public class HorizontalMovementSystem : IExecuteSystem  
+{
+    private readonly Contexts _contexts;
+    private readonly IGroup<GameEntity> _movingEntities;
+
+    public HorizontalMovementSystem(Contexts contexts)
+    {
+        _contexts = contexts;
+        _movingEntities = contexts.game.GetGroup(GameMatcher.HorizontallyMoving);
+    }
+
+    public void Execute() 
+    {
+        foreach (GameEntity movingEntity in _movingEntities)
+        {
+            movingEntity.ReplacePosition(new Vector3(
+                Mathf.Cos(Time.time),
+                movingEntity.position.position.y,
+                movingEntity.position.position.z
+            ));
+        }
+    }
+}
