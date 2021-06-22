0e857a1dc922e2000ee5aa1bec2446e6d67533c2
Oguz Konya <oguzkonya34@gmail.com>
2021-06-22 23:45:14 +0100

    Add game controller

---
+++ Assets/Scripts/Controllers/GameController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Entitas;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+	private Systems systems;
+
+	private void Start()
+	{
+		systems = new Feature("systems")
+			.Add(new InitializeGameSystem(Contexts.sharedInstance))
+
+			.Add(new HorizontalMovementSystem(Contexts.sharedInstance))
+			;
+
+		systems.Initialize();
+	}
+
+	private void Update()
+	{
+		systems.Execute();
+	}
+
+	private void OnDestroy()
+	{
+		systems.TearDown();
+	}
+}
